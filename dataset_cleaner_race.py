ba2c844fc7e1cce37b98576acc2ff8b2f033282d
grauso-t <grausoteodoro@gmail.com>
2024-01-19 10:50:05 +0100

    Dataset Cleaner

---
+++ dataset_cleaner_race.py
@@ -0,0 +1,30 @@
+import pandas as pd
+
+lap_times = pd.read_csv(r'./Datasets/lap_times.csv', low_memory=False)
+results = pd.read_csv(r'./Datasets/results.csv', low_memory=False)
+drivers = pd.read_csv(r'./Datasets/drivers.csv', low_memory=False)
+races = pd.read_csv(r'./Datasets/races.csv', low_memory=False)
+status = pd.read_csv(r'./Datasets/status.csv', low_memory=False)
+
+merge1 = pd.merge(lap_times, drivers, how='left', on=['driverId'])
+merge1 = merge1.rename(columns={'position': f'position_lap'})
+merge1 = merge1.rename(columns={'time': f'time_lap'})
+merge1 = merge1.rename(columns={'milliseconds': f'milliseconds_lap'})
+merge1 = merge1.rename(columns={'number': f'number_driver'})
+merge1 = merge1.drop('url', axis=1)
+merge1 = merge1.drop('driverRef', axis=1)
+merge1['driver_name'] = merge1['forename'] + ' ' + merge1['surname']
+merge1 = merge1.drop(['forename', 'surname'], axis=1)
+merge1 = merge1.drop(['dob', 'nationality'], axis=1)
+merge1 = merge1.drop(['number_driver', 'code'], axis=1)
+
+merge2 = pd.merge(merge1, races, how='left', on=['raceId'])
+merge2 = merge2.rename(columns={'time': f'time_race'})
+merge2 = merge2.drop(['year', 'round', 'url', 'fp1_date', 'fp1_time', 'fp2_date', 'fp2_time', 'fp3_date', 'fp3_time', 'quali_date', 'quali_time', 'sprint_date', 'sprint_time'], axis=1)
+
+merge3 = pd.merge(merge2, results, how='left', on=['raceId', 'driverId'])
+merge3 = merge3.drop(['constructorId', 'circuitId'], axis=1)
+
+merge4 = pd.merge(merge3, status, how='left', on=['statusId'])
+
+merge4.to_csv('merged_dataset_races.csv', index=False)
--- predictor.py
+++ predictor.py
@@ -1,42 +1,29 @@
-from datetime import timedelta
 import pandas as pd
+import matplotlib.pyplot as plt
 
-# Carica i dati dai file CSV
-results = pd.read_csv(r'./Datasets/results.csv')
-races = pd.read_csv(r'./Datasets/races.csv')
-qualifying = pd.read_csv(r'./Datasets/qualifying.csv')
-drivers = pd.read_csv(r'./Datasets/drivers.csv')
-constructors = pd.read_csv(r'./Datasets/constructors.csv')
-circuit = pd.read_csv(r'./Datasets/circuits.csv')
+dataset = pd.read_csv(r'./merged_dataset_races.csv', low_memory=False)
 
-# Esegue le fusioni dei dataframe
-df1 = pd.merge(races, results, how='inner', on=['raceId'])
-df2 = pd.merge(df1, qualifying, how='inner', on=['raceId', 'driverId', 'constructorId'])
-df3 = pd.merge(df2, drivers, how='inner', on=['driverId'])
-df4 = pd.merge(df3, constructors, how='inner', on=['constructorId'])
-df5 = pd.merge(df4, circuit, how='inner', on=['circuitId'], suffixes=('_df4', '_circuit'))
+dataset['date'] = pd.to_datetime(dataset['date'])
 
-# Rimuove le colonne specificate
-columns_to_remove = ['round', 'time_x', 'fp1_time', 'fp2_time', 'fp3_time', 'sprint_date', 'sprint_time', 'laps', 'forename', 'surname', 'nationality_x', 'nationality_y', 'url_circuit', 'url_x', 'resultId', 'quali_time', 'fp1_date', 'fp2_date', 'fp3_date', 'number_x', 'positionText', 'position_x', 'positionOrder', 'driverRef', 'number', 'dob', 'url_y', 'name_y', 'url_df4', 'time_y']
-df5 = df5.drop(columns=columns_to_remove, errors='ignore')
+# Filtra il DataFrame eliminando le righe con 'date' minore di '2020-01-01'
+dataset = dataset[dataset['date'] >= '2022-01-01']
 
-# Converte la colonna 'date' al formato datetime
-df5['date'] = pd.to_datetime(df5['date'], errors='coerce')
+nan_mask = dataset.applymap(lambda x: x == '\\N')
+nan_count = nan_mask.sum()
 
-# Identifica le righe con "\N" nella colonna 'quali_date'
-rows_with_NA = df5[df5['quali_date'] == '\\N']
+print(nan_count)
 
-# Converte la colonna 'date' al formato datetime solo per le righe con "\N" nella colonna 'quali_date'
-df5.loc[rows_with_NA.index, 'date'] = pd.to_datetime(df5.loc[rows_with_NA.index, 'date'], errors='coerce')
+# Raggruppa per 'driverId' e conta le occorrenze
+conteggio_per_id = dataset.groupby('driverId').size().reset_index(name='numero_occorrenze')
 
-# Sottrai un giorno dalla colonna 'date' per le righe con "\N" nella colonna 'quali_date'
-df5.loc[rows_with_NA.index, 'quali_date'] = (df5.loc[rows_with_NA.index, 'date'] - pd.to_timedelta(1, unit='d'))
+# Crea un grafico a barre
+plt.bar(conteggio_per_id['driverId'], conteggio_per_id['numero_occorrenze'])
+plt.xlabel('Driver ID')
+plt.ylabel('Numero di Occorrenze')
+plt.title('Conteggio delle Occorrenze per Driver ID')
+plt.show()
 
-# Converte la colonna 'quali_date' al formato datetime
-df5['quali_date'] = pd.to_datetime(df5['quali_date'], errors='coerce')
+conteggio_per_id = dataset.groupby('driverId').size().reset_index(name='numero_occorrenze')
 
-# Converte la colonna 'quali_date' al formato stringa desiderato "YYYY/MM/DD"
-df5['quali_date'] = df5['quali_date'].dt.strftime('%Y-%m-%d')
-
-# Salva il dataframe modificato in un file CSV
-df5.to_csv('merged_dataset.csv', index=False)
+print(len(conteggio_per_id[(conteggio_per_id['numero_occorrenze'] > 2000)]))
+print(len(conteggio_per_id[(conteggio_per_id['numero_occorrenze'] <= 2000)]))
