84843c0829210784dc1963d2506dd4041b6f127a
grauso-t <grausoteodoro@gmail.com>
2024-01-31 12:37:16 +0100

    Meteo API

--- Scripts/dataAnalysis.py
+++ Scripts/dataAnalysis.py
@@ -8,39 +8,39 @@ import numpy as np
 # Load the dataset
 ds = pd.read_csv(r'./merged_dataset_races.csv', low_memory=False)
 
-lista = [1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 45, 50, 128, 53, 55, 58, 88, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 122, 123, 124, 125, 127, 133, 134]
+list = [1, 11, 12, 13, 14, 15, 16, 17, 18, 19, 45, 50, 128, 53, 55, 58, 88, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 122, 123, 124, 125, 127, 133, 134]
 
 # Count the number of rows where 'statusId' is in the given list
-count = len(ds[ds['statusId'].isin(lista)])
+count = len(ds[ds['statusId'].isin(list)])
 
 # Print the total count
-print("Ecco il numero di gare che non sono terminate precocemente: " + str(count))
+print("Number of completed laps: " + str(count))
 
 # Group by 'driverId' and 'driver_name' and sum the number of laps
-raggruppato_ds = ds.groupby(['driverId', 'driver_name']).size().reset_index(name='conteggio')
+grouped_ds = ds.groupby(['driverId', 'driver_name']).size().reset_index(name='conteggio')
 
 # Compute class weights
-class_weights = compute_class_weight('balanced', classes=np.unique(raggruppato_ds['conteggio']), y=raggruppato_ds['conteggio'])
-class_weight_dict = dict(zip(np.unique(raggruppato_ds['conteggio']), class_weights))
+class_weights = compute_class_weight('balanced', classes=np.unique(grouped_ds['conteggio']), y=grouped_ds['conteggio'])
+class_weight_dict = dict(zip(np.unique(grouped_ds['conteggio']), class_weights))
 
 # Display the resulting DataFrame
-print(raggruppato_ds)
+print(grouped_ds)
 
 # Save the grouped DataFrame to a CSV file
-raggruppato_ds.to_csv('raggruppato.csv', index=False)
+grouped_ds.to_csv('raggruppato.csv', index=False)
 
 # Print the number of drivers who have completed at least 40 laps
-print("Piloti che hanno effettuato almeno 40 giri: " + str(len(raggruppato_ds[(raggruppato_ds['conteggio'] >= 40)])))
+print("Piloti che hanno effettuato almeno 40 giri: " + str(len(grouped_ds[(grouped_ds['conteggio'] >= 40)])))
 
 # Print the number of drivers who have not completed at least 40 laps
-print("Piloti che non hanno effettuato almeno 40 giri: " + str(len(raggruppato_ds[(raggruppato_ds['conteggio'] < 40)])))
+print("Piloti che hanno effettuato meno di 40 giri: " + str(len(grouped_ds[(grouped_ds['conteggio'] < 40)])))
 
 # Print the total number of drivers
-print("Piloti totale: " + str(len(raggruppato_ds)))
+print("Piloti totale: " + str(len(grouped_ds)))
 
 # Apply oversampling specifically to the number of laps
-X = raggruppato_ds.drop('conteggio', axis=1)
-y = raggruppato_ds['conteggio']
+X = grouped_ds.drop('conteggio', axis=1)
+y = grouped_ds['conteggio']
 
 ros = RandomOverSampler(sampling_strategy='auto', random_state=42)
 X_resampled, y_resampled = ros.fit_resample(X, y)
--- Scripts/dataset_cleaner_race.py
+++ Scripts/dataset_cleaner_race.py
@@ -1,21 +1,20 @@
 import pandas as pd
+import openmeteo_requests
+import requests_cache
+
+from retry_requests import retry
 
 lap_times = pd.read_csv(r'./Datasets/lap_times.csv', low_memory=False)
 results = pd.read_csv(r'./Datasets/results.csv', low_memory=False)
 drivers = pd.read_csv(r'./Datasets/drivers.csv', low_memory=False)
 races = pd.read_csv(r'./Datasets/races.csv', low_memory=False)
 status = pd.read_csv(r'./Datasets/status.csv', low_memory=False)
+circuits = pd.read_csv(r'./Datasets/circuits.csv', low_memory=False)
 
 merge1 = pd.merge(lap_times, drivers, how='left', on=['driverId'])
-merge1 = merge1.rename(columns={'position': f'position_lap'})
-merge1 = merge1.rename(columns={'milliseconds': f'milliseconds_lap'})
-merge1 = merge1.drop(['url', 'time'], axis=1)
-merge1 = merge1.rename(columns={'number': f'number_driver', 'milliseconds_lap': f'time_lap'})
-merge1 = merge1.drop('driverRef', axis=1)
 merge1['driver_name'] = merge1['forename'] + ' ' + merge1['surname']
-merge1 = merge1.drop(['forename', 'surname'], axis=1)
-merge1 = merge1.drop(['dob', 'nationality'], axis=1)
-merge1 = merge1.drop(['number_driver', 'code'], axis=1)
+merge1 = merge1.rename(columns={'number': f'number_driver', 'milliseconds': f'time_lap', 'position': f'position_lap'})
+merge1 = merge1.drop(['number_driver', 'code', 'url', 'time', 'driverRef', 'forename', 'surname', 'dob', 'nationality'], axis=1)
 
 merge2 = pd.merge(merge1, races, how='left', on=['raceId'])
 merge2 = merge2.rename(columns={'time': f'time_race'})
@@ -28,10 +27,94 @@ merge3 = merge3.drop(['constructorId', 'resultId','number', 'grid', 'position', 
 merge4 = pd.merge(merge3, status, how='left', on=['statusId'])
 merge4 = merge4.rename(columns={'date': f'race_date'})
 
-new_order = ['raceId', 'driverId', 'driver_name', 'circuitId', 'cirtuit_name', 'race_date', 'time_race', 'lap', 'position_lap', 'time_lap', 'statusId', 'status']
+merge5 = pd.merge(merge4, circuits, how='left', on=['circuitId'])
 
-merge4 = merge4[new_order]
+ds_final = merge5[merge5['race_date'] >= '2022-01-01']
 
-ds_final = merge4[merge4['race_date'] >= '2022-01-01']
+grouped_ds = ds_final.groupby('raceId')
 
-ds_final.to_csv('merged_dataset_races.csv', index=False)
+# Setup the Open-Meteo API client with cache and retry on error
+cache_session = requests_cache.CachedSession('.cache', expire_after=-1)
+retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
+openmeteo = openmeteo_requests.Client(session=retry_session)
+
+# Create an empty DataFrame to store the final results
+final_results = pd.DataFrame()
+
+# Iterate over groups
+for race_id, group_df in grouped_ds:
+
+    latitude = str(group_df['lat'].iloc[0])
+    longitude = str(group_df['lng'].iloc[0])
+    date = group_df['race_date'].iloc[0]
+    time_str = str(group_df['time_race'].iloc[0])
+    hour = int(time_str.split(':')[0])
+
+    url = "https://archive-api.open-meteo.com/v1/archive"
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "start_date": date,
+        "end_date": date,
+        "hourly": ["weather_code"]
+    }
+
+    responses = openmeteo.weather_api(url, params=params)
+    response = responses[0]
+    hourly = response.Hourly()
+    hourly_weather_code = hourly.Variables(0).ValuesAsNumpy()
+
+    hourly_data = {"date": pd.date_range(
+        start=pd.to_datetime(hourly.Time(), unit="s"),
+        end=pd.to_datetime(hourly.TimeEnd(), unit="s"),
+        freq=pd.Timedelta(seconds=hourly.Interval()),
+        inclusive="left"
+    )}
+    hourly_data["weather_code"] = hourly_weather_code
+    group_df['weather_code'] = hourly_data["weather_code"][hour]
+    
+    # Append the modified group DataFrame to the final results
+    final_results = final_results._append(group_df, ignore_index=True)
+
+wmo_mapping = {
+    0: 'Clear sky',
+    1: 'Mainly clear',
+    2: 'Partly cloudy',
+    3: 'Cloudy',
+    4: 'Overcast',
+    5: 'Mist',
+    10: 'Patchy fog',
+    45: 'Fog',
+    50: 'Freezing drizzle',
+    51: 'Drizzle',
+    52: 'Freezing rain',
+    53: 'Rain',
+    54: 'Snow',
+    55: 'Thunderstorm',
+    56: 'Sleet',
+    57: 'Hail',
+    58: 'Freezing spray',
+    60: 'Showers of rain',
+    61: 'Showers of snow',
+    62: 'Showers of rain and snow',
+    63: 'Showers of hail',
+    64: 'Showers of rain and hail',
+    65: 'Showers of snow and hail',
+    66: 'Showers of rain, snow, and hail',
+    67: 'Drizzle and rain',
+    68: 'Drizzle and snow',
+    70: 'Widespread dust',
+    71: 'Duststorm or sandstorm',
+    72: 'Volcanic ash',
+    73: 'Squalls',
+    74: 'Funnel cloud',
+    75: 'Tornado',
+}
+
+final_results['weather_description'] = final_results['weather_code'].map(wmo_mapping)
+
+new_order = ['raceId', 'driverId', 'driver_name', 'circuitId', 'cirtuit_name', 'race_date', 'time_race', 'lap', 'position_lap', 'time_lap', 'statusId', 'status', 'weather_code', 'weather_description']
+final_results = final_results[new_order]
+
+# Save the final results to a CSV file
+final_results.to_csv('merged_dataset_races.csv', index=False)
